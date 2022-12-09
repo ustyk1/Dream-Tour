7ec133525814ce527eff9e73c6c5edd5f5bcf9ca
ustyk1 <eugenia.ustyk@gmail.com>
2022-12-09 22:09:32 +0200

    fix mobile version bugs 2

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,10 +14,12 @@ $accordion.addEventListener('click', (e) => {
 
 const $hamburgerBtn = document.querySelector('.nav-btn');
 const $navigation = document.querySelector('.header__nav')
+const $html = document.documentElement;
 
 $hamburgerBtn.addEventListener('click', () => {
   $navigation.classList.toggle('active');
   $hamburgerBtn.classList.toggle('active');
+  $html.classList.toggle('active');
 });
 
 
